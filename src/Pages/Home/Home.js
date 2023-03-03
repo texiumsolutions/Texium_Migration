582e7f0665876379e976f21a106eab3911532d28
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2023-03-03 19:13:31 +0600

    Job Page UI Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 export const Home = () => {
   return (
     <div>
-      <h1>
-        WELCOME!{ " "   }
+      <h1> 
+        WELCOME!{" "}
         <small>
           <small>
             <small style={{color: "var(--primary)"}}>TEXIUM MIGRATION</small>
