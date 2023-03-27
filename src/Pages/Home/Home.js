c22e19b57c112e586d2563eb71c0048ca0da14ab
Nahim Hossain Shohan <nh.shohan6@gmail.com>
2023-03-27 23:19:10 +0600

    UI changed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -16,11 +16,20 @@ export const Home = () => {
 
       <div className="content_container">
         <Card height="calc(100%)" className="inter_card" width="calc(100%)">
-          {displayName && <h3 className="content_container_h3">{displayName}  <img src="https://i.ibb.co/WBwFw0W/pexels-anastasiya-gepp-1462636.jpg" alt="" /></h3>}
+          {displayName && (
+            <h3 className="content_container_h3">
+              {displayName}{" "}
+              <img
+                src="https://i.ibb.co/WBwFw0W/pexels-anastasiya-gepp-1462636.jpg"
+                alt=""
+              />
+            </h3>
+          )}
           <div className="home_body">
-          <h1 className="type_writer_h1 type_writer">Welcome To Texium Migration Platform !!!</h1>
+            <h1 className="type_writer_h1 type_writer">
+              Welcome To Texium Migration Platform !!!
+            </h1>
           </div>
-        
         </Card>
       </div>
     </div>
